0ce3201ec83414ae0e70ed40fbe252c28ba53512
rudi.catsmile <rudi.catsmile2@gmail.com>
2026-04-09 11:38:08 +0700

    refactor: rename create button label to 'Buat Baru' for clarity

--- app/Filament/Resources/FinancialRecords/Pages/ListFinancialRecords.php
+++ app/Filament/Resources/FinancialRecords/Pages/ListFinancialRecords.php
@@ -13,7 +13,7 @@ class ListFinancialRecords extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()->label('Buat Baru'),
         ];
     }
 }
